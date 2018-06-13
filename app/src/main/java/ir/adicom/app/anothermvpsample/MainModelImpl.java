909a39d698d7b953db1fd5f4d2b2d286c8116932
YasharP <adicom.ir@gmail.com>
2018-06-13 13:45:17 +0430

    Add logic to model

--- app/src/main/java/ir/adicom/app/anothermvpsample/MainModelImpl.java
+++ app/src/main/java/ir/adicom/app/anothermvpsample/MainModelImpl.java
@@ -1,12 +1,16 @@
 package ir.adicom.app.anothermvpsample;
 
+import java.util.Random;
+
 /**
+ *
  * Created by Yashar.P on 6/13/2018.
  */
 
 public class MainModelImpl implements MainModel {
     @Override
     public int getRandomGrade() {
-        return 0;
+        Random random = new Random(System.currentTimeMillis());
+        return (int) (random.nextFloat() * 20);
     }
 }
--- app/src/main/java/ir/adicom/app/anothermvpsample/MainView.java
+++ app/src/main/java/ir/adicom/app/anothermvpsample/MainView.java
@@ -1,6 +1,7 @@
 package ir.adicom.app.anothermvpsample;
 
 /**
+ *
  * Created by Yashar.P on 6/13/2018.
  */
 
